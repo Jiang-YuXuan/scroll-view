445a6d68b06d22d398e4a10489cf2c89df938eb7
Jiang-YuXuan <18706828703@163.com>
2019-11-21 12:04:37 +0800

    Create Classify.vue

---
+++ Classify.vue
@@ -0,0 +1,24 @@
+<template>
+  <scroll-view top="46px" bottom="50px">
+    <div id="page">this is classify</div>
+  </scroll-view>
+</template>
+<script>
+import ScrollView from "./ScrollView.vue";
+export default {
+  components: {
+    "scroll-view": ScrollView
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+<style scoped>
+#page {
+  width: 100%;
+  min-height: 100%;
+}
+</style>
